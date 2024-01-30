13a7eb535c66d8018cfb1cf2c7720531fe0b7f19
KevDaDev <65958288+KevinDaGame@users.noreply.github.com>
2024-01-30 07:08:01 +0100

    Init

---
+++ RemindoBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace RemindoBot;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+    
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        
+        //read env file
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+    
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
